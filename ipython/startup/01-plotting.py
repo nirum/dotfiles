30f521060d5898fb24cfd6f846902e7fb6e09acf
Niru Maheswaranathan <nirum@stanford.edu>
2015-09-30 17:40:01 -0700

    Cleaned up matplotlib fonts and imports

--- ipython/startup/01-plotting.py
+++ ipython/startup/01-plotting.py
@@ -2,17 +2,7 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-from palettable.tableau import Tableau_10
 plt.ion()
 
-# seaborn options
-import seaborn as sns
-sns.set(font='sans-serif')
-sns.set_style('white')
-sns.set_context('talk', font_scale=1.6)
-
-# color styles
-matplotlib.rc('lines', linewidth=3)
-matplotlib.rc('axes', color_cycle=Tableau_10.mpl_colors)
-matplotlib.rc('font', family='sans-serif')
-matplotlib.rc('font', serif='Helvetica Neue')
+# style (new in 1.4)
+plt.style.use('fivethirtyeight')

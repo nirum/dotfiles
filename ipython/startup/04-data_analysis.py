0934559f0e30dfcdd96c678e3d55643446abbae5
Niru Maheswaranathan <nirum@stanford.edu>
2015-10-11 10:36:31 -0700

    also import pi

--- ipython/startup/04-data_analysis.py
+++ ipython/startup/04-data_analysis.py
@@ -1,5 +1,5 @@
 # sanity
 from numpy.random import rand, randn, shuffle, choice, sample
-from numpy import arange, zeros, ones, eye, linspace
+from numpy import arange, zeros, ones, eye, linspace, pi
 from scipy.linalg import *
 from matplotlib.pyplot import *

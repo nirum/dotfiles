4ea68dfbc642986a6ad10d4479366d11782c5bb9
Niru Maheswaranathan <nirum@stanford.edu>
2015-10-15 22:30:55 -0400

    adding np.cov to default imports

--- ipython/startup/04-data_analysis.py
+++ ipython/startup/04-data_analysis.py
@@ -1,5 +1,5 @@
 # sanity
 from numpy.random import rand, randn, shuffle, choice, sample
-from numpy import arange, zeros, ones, eye, linspace, pi, inf, nan
+from numpy import arange, zeros, ones, eye, linspace, pi, inf, nan, cov
 from scipy.linalg import *
 from matplotlib.pyplot import *

e0a59205f9022da93afee315c842478d5c570d7f
Niru Maheswaranathan <nirum@stanford.edu>
2015-10-28 02:29:47 -0700

    adds array to default imports

--- ipython/startup/04-data_analysis.py
+++ ipython/startup/04-data_analysis.py
@@ -1,5 +1,5 @@
 # sanity
 from numpy.random import rand, randn, shuffle, choice, sample
-from numpy import arange, zeros, ones, eye, linspace, pi, inf, nan, cov
+from numpy import arange, zeros, ones, eye, linspace, pi, inf, nan, cov, array
 from scipy.linalg import *
 from matplotlib.pyplot import *

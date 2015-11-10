8f9a3d67bb15a5b64cd6516bb1238814888d9acb
Niru Maheswaranathan <nirum@stanford.edu>
2015-11-10 01:13:07 -0800

    python config updates

--- ipython/startup/04-data_analysis.py
+++ ipython/startup/04-data_analysis.py
@@ -1,5 +1,5 @@
 # sanity
 from numpy.random import rand, randn, shuffle, choice, sample
-from numpy import arange, zeros, ones, eye, linspace, pi, inf, nan, cov, array
+from numpy import arange, zeros, ones, eye, linspace, pi, inf, nan, cov, array, sqrt
 from scipy.linalg import *
 from matplotlib.pyplot import *

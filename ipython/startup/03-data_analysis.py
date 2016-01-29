86d304fadebc9d2027b4bcc39d0c6ea200abbd19
Niru Maheswaranathan <nirum@stanford.edu>
2016-01-29 14:46:19 -0800

    adds log to default imports

--- ipython/startup/03-data_analysis.py
+++ ipython/startup/03-data_analysis.py
@@ -1,5 +1,5 @@
 # sanity
 from numpy.random import rand, randn, shuffle, choice, sample
-from numpy import arange, zeros, ones, eye, linspace, pi, inf, nan, cov, array, sqrt
+from numpy import arange, zeros, ones, eye, linspace, pi, inf, nan, cov, array, sqrt, log
 from scipy.linalg import *
 from matplotlib.pyplot import *

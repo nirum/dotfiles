dfa212995d7f0686bcfab3b2f7fbebd26b94bced
Niru Maheswaranathan <nirum@stanford.edu>
2016-01-30 14:00:13 -0800

    adds kurtosis, skew, and zscore to default imports

--- ipython/startup/03-data_analysis.py
+++ ipython/startup/03-data_analysis.py
@@ -2,4 +2,5 @@
 from numpy.random import rand, randn, shuffle, choice, sample
 from numpy import arange, zeros, ones, eye, linspace, pi, inf, nan, cov, array, sqrt, log
 from scipy.linalg import *
+from scipy.stats import kurtosis, skew, zscore
 from matplotlib.pyplot import *

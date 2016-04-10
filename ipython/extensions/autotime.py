e6bfbf537b034ead5d852d602b5bcaf3210bde42
Niru Maheswaranathan <nirum@stanford.edu>
2016-04-10 13:41:11 -0700

    cleaning up autotime

--- ipython/extensions/autotime.py
+++ ipython/extensions/autotime.py
@@ -1,8 +1,8 @@
 from __future__ import print_function
 from IPython.core.magics.execution import _format_time as fmt
 import os
+NOTIFICATION_MSG=u"terminal-notifier -appIcon https://www.python.org/static/apple-touch-icon-144x144-precomposed.png -sound Submarine -message '\u23F1 {}' -title 'Complete \u2714'"
 
-# import perf_counter on py3, alias to time on py2
 try:
     from time import perf_counter
 except ImportError:
@@ -19,7 +19,6 @@ class Timer(object):
         thresholds : float
             Any times below the first threshold (in seconds) are not printed
             Times greater than the second threshold are shown as a desktop notification
-
         """
 
         self.start_time = 0.0
@@ -27,19 +26,11 @@ class Timer(object):
         self.theta_display = thresholds[1]
 
     def start(self):
-        """
-        Start the timer
-        uses the `perf_counter` function in the `time` module in python3
-
-        """
-
+        """Start the timer"""
         self.start_time = perf_counter()
 
     def stop(self):
-        """
-        Stops the timer, and prints the elapsed time
-
-        """
+        """Stops the timer, and prints the elapsed time"""
 
         if self.start_time:
             diff = perf_counter() - self.start_time
@@ -49,30 +40,20 @@ class Timer(object):
             if diff > self.theta_print:
                 print(u'\u23F1  {}'.format(fmt(diff)))
 
-            if diff > self.theta_display:
-                if os.uname().nodename == 'mbp':
-                    cmd = u"terminal-notifier -appIcon https://www.python.org/static/apple-touch-icon-144x144-precomposed.png -sound Submarine -message '\u23F1 {}' -title 'Complete \u2714'".format(fmt(diff))
-                    os.system(cmd)
-
-
-timer = Timer()
+            if diff > self.theta_display and os.uname().nodename == 'nirumbp':
+                os.system(NOTIFICATION_MSG.format(fmt(diff)))
 
 
 def load_ipython_extension(ip):
-    """
-    On load, start and stop the timer every time a cell is run
-
-    """
-
+    """On load, start and stop the timer every time a cell is run"""
     ip.events.register('pre_run_cell', timer.start)
     ip.events.register('post_run_cell', timer.stop)
 
 
 def unload_ipython_extension(ip):
-    """
-    On unload, remove the timer
-
-    """
-
+    """On unload, remove the timer"""
     ip.events.unregister('pre_run_cell', timer.start)
     ip.events.unregister('post_run_cell', timer.stop)
+
+
+timer = Timer()

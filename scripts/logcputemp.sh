e93a92e1dc8a4c02a02b96df5bb262d5afba4b68
Niru Maheswaranathan <niru@fastmail.com>
2019-04-14 10:49:03 -0700

    removing scripts

--- scripts/logcputemp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-while sleep 60;
-do { echo -n $(date +%s); echo -n ","; echo -n $(sensors | sed -rn 's/^.* \+([0-9]+)\.[0-9].C .*/\1/p' | xargs | sed -e 's/ /,/g'); echo -n ","; echo $(nvidia-smi -q -d temperature | sed -rn 's/^.*GPU Current.*: ([0-9]+).*/\1/p' | xargs | sed -e 's/ /,/g'); } >> /home/nirum/Dropbox/temperature.log
-done

bdba0585d7954bcb8dde4d141c71e72e880fc083
Niru Maheswaranathan <niru@hey.com>
2021-10-24 11:41:33 -0700

    Switches from vim-plug to packer.nvim

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,31 @@
+packer = require('packer')
+
+packer.startup(function(use) 
+  use 'wbthomason/packer.nvim'
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use 'tpope/vim-commentary'                      -- comments
+  use 'tpope/vim-surround'                        -- quotes/parens/brackets/etc
+  use 'lukas-reineke/indent-blankline.nvim'       -- indent guides
+  use 'norcalli/nvim-colorizer.lua'               -- color hex codes
+  use 'tpope/vim-surround'                        -- ()[]{}
+  use 'lukas-reineke/indent-blankline.nvim'       -- indent guides
+  use 'norcalli/nvim-colorizer.lua'               -- color hex codes
+  use 'lewis6991/gitsigns.nvim'                   -- git sign column
+  use 'tpope/vim-fugitive'                        -- git command support
+  use 'neovim/nvim-lspconfig'                     -- LSP
+  use 'nvim-lua/popup.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use 'hrsh7th/nvim-cmp'                          -- Completion Engine
+  use 'nvim-telescope/telescope.nvim'             -- Fuzzy Finding
+  use 'sbdchd/neoformat'                          -- yapf formatting
+  use 'folke/tokyonight.nvim'                     -- Theme
+  use 'kyazdani42/nvim-web-devicons'              -- Icons
+  use 'nvim-lualine/lualine.nvim'                 -- Status line
+end)
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])

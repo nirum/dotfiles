9e31d6499a36b4851d28a566614928c4cef512af
Niru Maheswaranathan <niru@fb.com>
2021-10-25 17:39:09 -0700

    Removes duplicate plugin entries.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -7,15 +7,16 @@ packer = require('packer')
 packer.startup(function(use) 
   use 'wbthomason/packer.nvim'
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
   use 'tpope/vim-commentary'                      -- comments
   use 'tpope/vim-surround'                        -- quotes/parens/brackets/etc
+
   use 'lukas-reineke/indent-blankline.nvim'       -- indent guides
   use 'norcalli/nvim-colorizer.lua'               -- color hex codes
-  use 'tpope/vim-surround'                        -- ()[]{}
-  use 'lukas-reineke/indent-blankline.nvim'       -- indent guides
-  use 'norcalli/nvim-colorizer.lua'               -- color hex codes
+
   use 'lewis6991/gitsigns.nvim'                   -- git sign column
   use 'tpope/vim-fugitive'                        -- git command support
+
   use 'neovim/nvim-lspconfig'                     -- LSP
   use 'nvim-lua/popup.nvim'
   use 'nvim-lua/plenary.nvim'

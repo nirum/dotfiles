8da89b4c09b7f3c77d88b1c76434a47ceb55a8a8
Niru Maheswaranathan <niru@fb.com>
2021-12-21 14:57:57 -0800

    Shortcut for vim.opt

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,43 +1,45 @@
+o = vim.opt
+
 -- User 24 bit color in the terminal.
-vim.opt.termguicolors = true
+o.termguicolors = true
 
 -- Use relative line numbers.
-vim.opt.relativenumber = true
+o.relativenumber = true
 
 -- Show the absolute number of the current line.
-vim.opt.number = true
+o.number = true
 
 -- Use system clipboard.
-vim.opt.clipboard = "unnamedplus"
+o.clipboard = "unnamedplus"
 
 -- Turn on global ("g" flag) for substitutions (:s//g).
-vim.opt.gdefault = true
+o.gdefault = true
 
 -- Ignores case in search patterns.
-vim.opt.ignorecase = true
+o.ignorecase = true
 
 -- Override the ignorecase option if the search contains uppercase letters.
-vim.opt.smartcase = true
+o.smartcase = true
 
 -- Position splits to the right and below for vertical and regular splits.
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+o.splitright = true
+o.splitbelow = true
 
 -- On start, open all folds.
-vim.opt.foldlevelstart = 99
+o.foldlevelstart = 99
 
 -- Always show the sign column.
-vim.opt.signcolumn = 'yes'
+o.signcolumn = 'yes'
 
 -- Number of spaces a <Tab> counts for.
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+o.tabstop = 2
+o.shiftwidth = 2
 
 -- Replace <Tab> with spaces in insert mode.
-vim.opt.expandtab = true
+o.expandtab = true
 
 -- Try to be smart about indents on new lines.
-vim.opt.smartindent = false
+o.smartindent = false
 
 -- Use an undo file.
-vim.opt.undofile = true
+o.undofile = true

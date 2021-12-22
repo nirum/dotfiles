5f38ae7f106d752e010ca4160f9be22d40aa48ca
Niru Maheswaranathan <niru@fb.com>
2021-12-21 16:18:23 -0800

    Uses a dict for options.

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,22 +1,21 @@
-o = vim.opt
-
 local options = {
-  o.termguicolors = true        -- User 24 bit color in the terminal.
-  o.relativenumber = true       -- Use relative line numbers.
-  o.number = true               -- Show the absolute number of the current line.
-  o.clipboard = "unnamedplus"   -- Use system clipboard.
-  o.gdefault = true             -- Turn on global ("g" flag) for substitutions (:s//g).
-  o.ignorecase = true           -- Ignores case in search patterns.
-  o.smartcase = true            -- Override the ignorecase option if the search contains uppercase letters.
-  o.splitright = true           -- Position horizontal splits to the right.
-  o.splitbelow = true           -- Position vertical splits to the bottom.
-  o.foldlevelstart = 99         -- On start, open all folds.
-  o.signcolumn = 'yes'          -- Always show the sign column.
-  o.tabstop = 2                 -- Number of spaces a <Tab> counts for.
-  o.shiftwidth = 2              -- Number of spaces a <Tab> counts for.
-  o.expandtab = true            -- Replace <Tab> with spaces in insert mode.
-  o.smartindent = false         -- Try to be smart about indents on new lines.
-  o.undofile = true             -- Use an undo file.
+  termguicolors = true,       -- User 24 bit color in the terminal.
+  relativenumber = true,      -- Use relative line numbers.
+  number = true,              -- Show the absolute number of the current line.
+  clipboard = "unnamedplus",  -- Use system clipboard.
+  gdefault = true,            -- Turn on global ("g" flag) for substitutions (:s//g).
+  ignorecase = true,          -- Ignores case in search patterns.
+  smartcase = true,           -- Override the ignorecase option if the search contains uppercase letters.
+  splitright = true,          -- Position horizontal splits to the right.
+  splitbelow = true,          -- Position vertical splits to the bottom.
+  foldlevelstart = 99,        -- On start, open all folds.
+  signcolumn = 'yes',         -- Always show the sign column.
+  tabstop = 2,                -- Number of spaces a <Tab> counts for.
+  shiftwidth = 2,             -- Number of spaces a <Tab> counts for.
+  expandtab = true,           -- Replace <Tab> with spaces in insert mode.
+  smartindent = false,        -- Try to be smart about indents on new lines.
+  undofile = true,            -- Use an undo file.
+  scrolloff = 8,              -- Padding when scrolling.
 }
 
 for k, v in pairs(options) do

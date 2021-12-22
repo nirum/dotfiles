3e008cd842c37fc53e298c7482a7aa9013d3401b
Niru Maheswaranathan <niru@fb.com>
2021-12-21 16:14:29 -0800

    Updates mappings.

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -1,28 +1,33 @@
--- Shorthand.
--- local keymap = vim.api.nvim_set_keymap
--- local opts = { noremap = true, silent = true }
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
 
--- -- Remap space as leader key.
--- keymap("", "<Space>", "<Nop>", opts)
--- vim.g.mapleader = " "
--- vim.g.maplocalleader = " "
+-- Remap space as leader key.
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
--- -- Swap colon and semicolon.
--- keymap('', ';', ':', opts)
--- keymap('', ':', ';', opts)
+-- Swap colon and semicolon.
+keymap('', ';', ':', opts)
+keymap('', ':', ';', opts)
 
--- -- Clear highlighting..
+-- Clear highlighting..
 -- keymap('n', '<CR>', ':noh<CR>', opts)
 -- keymap('n', '<Esc>', ':noh<Esc>', opts)
 
--- -- Tab navigation.
--- keymap('n', '<tab>', ':tabnext<CR>', opts)
--- keymap('n', '<S-tab>', ':tabprevious<CR>', opts)
+-- Window navigation.
+keymap('n', '<C-h>', '<C-w>h', opts)
+keymap('n', '<C-j>', '<C-w>j', opts)
+keymap('n', '<C-k>', '<C-w>k', opts)
+keymap('n', '<C-l>', '<C-w>l', opts)
 
--- -- Visual mode persistent indenting.
--- keymap('v', '<', '<gv', opts)
--- keymap('v', '>', '>gv', opts)
+-- Tab navigation.
+keymap('n', '<tab>', ':tabnext<CR>', opts)
+keymap('n', '<S-tab>', ':tabprevious<CR>', opts)
+
+-- Visual mode persistent indenting.
+keymap('v', '<', '<gv', opts)
+keymap('v', '>', '>gv', opts)
 
 -- -- Move text up and down when visually selected.
--- keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
--- keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,45 +1,24 @@
 o = vim.opt
 
--- User 24 bit color in the terminal.
-o.termguicolors = true
+local options = {
+  o.termguicolors = true        -- User 24 bit color in the terminal.
+  o.relativenumber = true       -- Use relative line numbers.
+  o.number = true               -- Show the absolute number of the current line.
+  o.clipboard = "unnamedplus"   -- Use system clipboard.
+  o.gdefault = true             -- Turn on global ("g" flag) for substitutions (:s//g).
+  o.ignorecase = true           -- Ignores case in search patterns.
+  o.smartcase = true            -- Override the ignorecase option if the search contains uppercase letters.
+  o.splitright = true           -- Position horizontal splits to the right.
+  o.splitbelow = true           -- Position vertical splits to the bottom.
+  o.foldlevelstart = 99         -- On start, open all folds.
+  o.signcolumn = 'yes'          -- Always show the sign column.
+  o.tabstop = 2                 -- Number of spaces a <Tab> counts for.
+  o.shiftwidth = 2              -- Number of spaces a <Tab> counts for.
+  o.expandtab = true            -- Replace <Tab> with spaces in insert mode.
+  o.smartindent = false         -- Try to be smart about indents on new lines.
+  o.undofile = true             -- Use an undo file.
+}
 
--- Use relative line numbers.
-o.relativenumber = true
-
--- Show the absolute number of the current line.
-o.number = true
-
--- Use system clipboard.
-o.clipboard = "unnamedplus"
-
--- Turn on global ("g" flag) for substitutions (:s//g).
-o.gdefault = true
-
--- Ignores case in search patterns.
-o.ignorecase = true
-
--- Override the ignorecase option if the search contains uppercase letters.
-o.smartcase = true
-
--- Position splits to the right and below for vertical and regular splits.
-o.splitright = true
-o.splitbelow = true
-
--- On start, open all folds.
-o.foldlevelstart = 99
-
--- Always show the sign column.
-o.signcolumn = 'yes'
-
--- Number of spaces a <Tab> counts for.
-o.tabstop = 2
-o.shiftwidth = 2
-
--- Replace <Tab> with spaces in insert mode.
-o.expandtab = true
-
--- Try to be smart about indents on new lines.
-o.smartindent = false
-
--- Use an undo file.
-o.undofile = true
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end

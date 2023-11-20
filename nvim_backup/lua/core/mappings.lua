dbdef44d26f4be29934e95882623058f500f3c28
Niru Maheswaranathan <niru@hey.com>
2023-11-20 00:53:40 -0800

    [wip]

--- nvim_backup/lua/core/mappings.lua
+++ nvim_backup/lua/core/mappings.lua
@@ -1,3 +1,7 @@
+-- mappings.lua
+-- Author: Niru Maheswaranathan
+-- Website: https://github.com/nirum/dotfiles
+
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
@@ -32,3 +36,14 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
+-- Format (Neoformat)
+-- keymap("n", "<leader>f", ":Neoformat <CR>", opts)
+-- vim.cmd([[
+--   let g:neoformat_python_ufmt = {'exe': 'ufmt', 'args': ['format'], 'replace': 1}
+--   let g:neoformat_python_tidy = {'exe': 'tidy-imports', 'args': ['--black', '--quiet', '--replace-star-imports', '--action REPLACE'], 'replace': 1}
+--   let g:neoformat_enabled_python = ['tidy', 'ufmt']
+--   let g:neoformat_run_all_formatters = 1
+--   let g:neoformat_only_msg_on_error = 0
+-- ]])
+

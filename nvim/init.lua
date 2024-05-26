06dc70bab72bd6d38aebb0e894b27b1eaf775a21
Niru Maheswaranathan <niru@hey.com>
2024-05-26 15:42:38 -0700

    Removes mason, cleans up LSP, adds border to floating windows.

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,18 @@
 
 require("config.options")
 require("config.mappings")
+require("config.interface")
 require("config.plugins")
 
 vim.cmd([[colorscheme tokyonight]])
+
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   callback = function()
+--     vim.lsp.start({
+--       name = "clangd",
+--       cmd = {"clangd",},
+--       root_dir = vim.fn.getcwd(),
+--     })
+--   end,
+-- })
+--
--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -9,64 +9,65 @@ return {
 
   config = function()
     local lspconfig = require("lspconfig")
-    local ts = require("telescope.builtin")
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-    local on_attach = function(_, bufnr)
-      -- Helper function to define keybindings.
-      local nmap = function(keys, func, desc)
-        if desc then
-          desc = "LSP: " .. desc
-        end
-
-        vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc, noremap = true, silent = true })
-      end
-
-      -- Keybindings
-      nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-      nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-      nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-      nmap("gr", ts.lsp_references, "[G]oto [R]eferences")
-      nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-      nmap("<leader>ds", ts.lsp_document_symbols, "[D]ocument [S]ymbols")
-      nmap("<leader>ws", ts.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-      nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-      nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-      nmap("[d", vim.diagnostic.goto_prev, "Jump to previous diagnostic")
-      nmap("]d", vim.diagnostic.goto_next, "Jump to next diagnostic")
-      nmap("<leader>D", ts.diagnostics, "List [D]iagnostics for file")
-      nmap("<leader>d", vim.diagnostic.open_float, "Show [d]iagnostic for line")
-    end
-
-    -- Used to enable autocompletion (assign to every lsp server config).
-    local capabilities = cmp_nvim_lsp.default_capabilities()
+    -- local ts = require("telescope.builtin")
+    -- local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    --
+    -- local on_attach = function(_, bufnr)
+    --   -- Helper function to define keybindings.
+    --   local nmap = function(keys, func, desc)
+    --     if desc then
+    --       desc = "LSP: " .. desc
+    --     end
+    --
+    --     vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc, noremap = true, silent = true })
+    --   end
+    --
+    --   -- Keybindings
+    --   nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+    --   nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+    --   nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition") *
+    --   nmap("gr", ts.lsp_references, "[G]oto [R]eferences") *
+    --   nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation") ?
+    --   nmap("<leader>ds", ts.lsp_document_symbols, "[D]ocument [S]ymbols") *
+    --   nmap("<leader>ws", ts.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols") ?
+    --   nmap("K", vim.lsp.buf.hover, "Hover Documentation") *
+    --   nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation") ?
+    --   nmap("[d", vim.diagnostic.goto_prev, "Jump to previous diagnostic") *
+    --   nmap("]d", vim.diagnostic.goto_next, "Jump to next diagnostic") *
+    --   nmap("<leader>D", ts.diagnostics, "List [D]iagnostics for file") *
+    --   nmap("<leader>d", vim.diagnostic.open_float, "Show [d]iagnostic for line") *
+    -- end
+    --
+    -- -- Used to enable autocompletion (assign to every lsp server config).
+    -- local capabilities = cmp_nvim_lsp.default_capabilities()
 
     -- Enable the following language servers
-    local servers = { "ruff", "pylsp", "clangd", "rust_analyzer", "tsserver", "jsonls" }
+    -- local servers = { "ruff", "pylsp", "clangd", "rust_analyzer", "tsserver", "jsonls" }
+    local servers = { "clangd", "ruff", "tsserver", "pylsp", "rust_analyzer", "lua_ls", }
     for _, server in ipairs(servers) do
-      lspconfig[server].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
+      lspconfig[server].setup({})
+        -- on_attach = on_attach,
+        -- capabilities = capabilities,
+      -- })
     end
 
     -- configure lua server with custom settings
-    lspconfig["lua_ls"].setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { "vim" },
-          },
-          workspace = {
-            library = {
-              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-              [vim.fn.stdpath("config") .. "/lua"] = true,
-            },
-          },
-        },
-      },
-    })
+    -- lspconfig["lua_ls"].setup({
+    --   on_attach = on_attach,
+    --   capabilities = capabilities,
+    --   settings = {
+    --     Lua = {
+    --       diagnostics = {
+    --         globals = { "vim" },
+    --       },
+    --       workspace = {
+    --         library = {
+    --           [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+    --           [vim.fn.stdpath("config") .. "/lua"] = true,
+    --         },
+    --       },
+    --     },
+    --   },
+    -- })
   end,
 }

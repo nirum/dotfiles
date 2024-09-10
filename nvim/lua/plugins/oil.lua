05cf1ccebe86e934586fa7f4da89dd46b899b98c
Niru Maheswaranathan <niru@hey.com>
2024-09-10 11:27:49 -0700

    Updates to oil.nvim config

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -7,18 +7,24 @@ return {
         columns = { "icon" },
         keymaps = {
           ["<C-h>"] = false,
-          ["<M-h>"] = "actions.select_split",
+          ["-"] = false,
         },
         view_options = {
           show_hidden = true,
         },
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
+        prompt_save_on_select_new_entry = false,
+        float = {
+          padding = 10,
+        }
       })
 
       -- Open parent directory in current window
-      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+      vim.keymap.set("n", "<leader>-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
       -- Open parent directory in floating window
-      vim.keymap.set("n", "<leader>-", require("oil").toggle_float)
+      vim.keymap.set("n", "-", require("oil").toggle_float)
     end,
   },
 }
